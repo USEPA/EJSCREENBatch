ebec0e7c57fb49f50489c92d900415f003f8b162
Adam Theising <52790883+adamtheising@users.noreply.github.com>
2021-06-30 10:54:29 -0400

    Update EJMaps.R

--- R/EJMaps.R
+++ R/EJMaps.R
@@ -5,7 +5,7 @@
 #'
 #' @param input_data
 #' @param indic_option Options are 'total', 'environmental','demographic'. 'total' is default.
-#' @param perc_geog
+#' @param perc_geog Options are 'US' or 'state'.
 #' @param save_options
 #'
 #' @return
@@ -13,8 +13,7 @@
 #'
 #' @examples
 #' maps <- EJMaps(input_data = z, perc_geog = 'US', save.option = F)
-EJMaps <- function(input_data, indic_option = NULL, perc_geog = NULL,
-                   input.name = NULL, save_options = F){
+EJMaps <- function(input_data, indic_option = NULL, perc_geog = NULL, save_options = F){
 
   ## 3 possible color schemes: by env., demo. or total above 80th
   if(is.null(indic_option)){
@@ -31,130 +30,93 @@ EJMaps <- function(input_data, indic_option = NULL, perc_geog = NULL,
     geog.ind <- 'US'
   } else if (perc_geog %in% c('US', 'state')){
     geog.ind <- perc_geog
-  } else{
+  } else {
     stop('Accepted geographies are US and state.')
   }
 
   EJ.maps <- list()
 
   for (i in 1:length(input_data$EJ.facil.data)){
-    # Colors:
+    
+    map.data <- input_data$EJ.facil.data[[i]] %>%
+      dplyr::mutate(`Total indicators above 80th %ile` = as.factor(
+        as.numeric(as.character(`Env. indicators above 80th %ile`)) +
+          as.numeric(as.character(`Demo. indicators above 80th %ile`))
+      )) %>% 
+      dplyr::filter(!is.na(geometry)) %>%
+      dplyr::filter(geography == perc_geog) %>%
+      st_as_sf(crs = 4326)
+    
     if (ind.option == 'total'){
-      map.data <- input_data$EJ.facil.data[[i]] %>%
-        dplyr::mutate(`Total indicators above 80th %ile` = as.factor(
-          as.numeric(as.character(`Env. indicators above 80th %ile`)) +
-            as.numeric(as.character(`Demo. indicators above 80th %ile`))
-        )
-        )
-
-      # Add facility name if given by user.
-      if(!is.null(input.name)){
-        name.id <- as.data.frame(input.name) %>%
-          tibble::rowid_to_column('shape_ID')
-        names(name.id)[2] <- 'LocationID'
-        map.data <- map.data %>%
-          dplyr::inner_join(name.id) %>%
-          dplyr::relocate(LocationID)
-      }
-
       # Color palette
       pal <- leaflet::colorFactor(
-        rev(RColorBrewer::brewer.pal(n=11, "Spectral")),
-        domain = na.omit(map.data)$`Total indicators above 80th %ile`)
+        rev(RColorBrewer::brewer.pal(n=16, "Spectral")),
+        domain = map.data$`Total indicators above 80th %ile`)
 
       # Leaflet object
       EJ.maps[[names(input_data$EJ.facil.data)[i]]] <-
-        leaflet::leaflet(data = sf::st_as_sf(dplyr::filter(na.omit(map.data), geography == 'US'),
-                                crs = 4326)) %>% 
+        leaflet::leaflet(data = map.data) %>% 
         leaflet::addTiles() %>%
-        leaflet::fitBounds(lng1 = min(sf::st_coordinates(sf::st_as_sf(map.data))[,1]),
-                  lat1 = min(sf::st_coordinates(sf::st_as_sf(map.data))[,2]),
-                  lng2 = max(sf::st_coordinates(sf::st_as_sf(map.data))[,1]),
-                  lat2 = max(sf::st_coordinates(sf::st_as_sf(map.data))[,2])) %>%
+        leaflet::fitBounds(lng1 = min(sf::st_coordinates(map.data)[,1]),
+                  lat1 = min(sf::st_coordinates(map.data)[,2]),
+                  lng2 = max(sf::st_coordinates(map.data)[,1]),
+                  lat2 = max(sf::st_coordinates(map.data)[,2])) %>%
         leaflet::addCircleMarkers(radius = 5,
                          color = ~ pal(`Total indicators above 80th %ile`),
                          opacity = 0.75,
-                         popup = leafpop::popupTable(dplyr::filter(na.omit(map.data), geography == geog.ind),
+                         popup = leafpop::popupTable(map.data,
                                             feature.id = F, row.numbers = F,
-                                            zcol = names(na.omit(map.data))[1:20])) %>%
+                                            zcol = names(map.data)[1:20])) %>%
         leaflet::addLegend(pal = pal, values = ~`Total indicators above 80th %ile`, position = "bottomright")
     }
 
     if (ind.option == 'environmental'){
 
-      map.data <- input_data$EJ.facil.data[[i]]
-
-      # Add facility name if given by user.
-      if(!is.null(input.name) &
-         (length(input.name) == dim(input_data$EJ.facil.data[[i]])[1]/2)){
-        name.id <- as.data.frame(input.name) %>%
-          tibble::rowid_to_column('shape_ID')
-        names(name.id)[2] <- 'LocationID'
-        map.data <- map.data %>%
-          dplyr::inner_join(name.id) %>%
-          dplyr::relocate(LocationID)
-      }
-
       # Color palette
       pal <- leaflet::colorFactor(
-        rev(RColorBrewer::brewer.pal(n=11, "Spectral")),
-        domain = na.omit(input_data$EJ.facil.data[[i]])$`Env. indicators above 80th %ile`)
+        rev(RColorBrewer::brewer.pal(n=10, "Spectral")),
+        domain = map.data$`Env. indicators above 80th %ile`)
 
       # Leaflet object
       EJ.maps[[names(input_data$EJ.facil.data)[i]]] <-
-        leaflet::leaflet(data = sf::st_as_sf(dplyr::filter(na.omit(input_data$EJ.facil.data[[i]]), geography == 'US'),
-                                crs = 4326)) %>% 
+        leaflet::leaflet(data = map.data) %>% 
         leaflet::addTiles() %>%
-        leaflet::fitBounds(lng1 = min(sf::st_coordinates(sf::st_as_sf(input_data$EJ.facil.data[[i]]))[,1]),
-                  lat1 = min(sf::st_coordinates(sf::st_as_sf(input_data$EJ.facil.data[[i]]))[,2]),
-                  lng2 = max(sf::st_coordinates(sf::st_as_sf(input_data$EJ.facil.data[[i]]))[,1]),
-                  lat2 = max(sf::st_coordinates(sf::st_as_sf(input_data$EJ.facil.data[[i]]))[,2])) %>%
+        leaflet::fitBounds(lng1 = min(sf::st_coordinates(map.data)[,1]),
+                           lat1 = min(sf::st_coordinates(map.data)[,2]),
+                           lng2 = max(sf::st_coordinates(map.data)[,1]),
+                           lat2 = max(sf::st_coordinates(map.data)[,2])) %>%
         leaflet::addCircleMarkers(radius = 5,
                          color = ~ pal(`Env. indicators above 80th %ile`),
                          opacity = 0.75,
                          #clusterOptions = markerClusterOptions(),
-                         popup = leafpop::popupTable(dplyr::filter(na.omit(input_data$EJ.facil.data[[i]]), geography == geog.ind),
+                         popup = leafpop::popupTable(map.data,
                                             feature.id = F, row.numbers = F,
-                                            zcol = names(na.omit(input_data$EJ.facil.data[[i]]))[1:20])) %>%
+                                            zcol = names(map.data[1:20])) %>%
         leaflet::addLegend(pal = pal, values = ~`Env. indicators above 80th %ile`, position = "bottomright")
     }
 
     if (ind.option == 'demographic'){
 
-      map.data <- input_data$EJ.facil.data
-
-      # Add facility name if given by user.
-      if(!is.null(input.name) &
-         (length(input.name) == dim(input_data$EJ.facil.data[[i]])[1]/2)){
-        name.id <- as.data.frame(input.name) %>%
-          tibble::rowid_to_column('shape_ID')
-        names(name.id)[2] <- 'LocationID'
-        map.data <- map.data %>%
-          dplyr::inner_join(name.id) %>%
-          dplyr::relocate(LocationID)
-      }
-
       # Color palette
       pal <- leaflet::colorFactor(
-        rev(RColorBrewer::brewer.pal(n=11, "Spectral")),
-        domain = na.omit(input_data$EJ.facil.data[[i]])$`Demo. indicators above 80th %ile`)
+        rev(RColorBrewer::brewer.pal(n=6, "Spectral")),
+        domain = map.data$`Demo. indicators above 80th %ile`)
 
       # Leaflet object
       EJ.maps[[names(input_data$EJ.facil.data)[i]]] <-
-        leaflet::leaflet(data = sf::st_as_sf(dplyr::filter(na.omit(input_data$EJ.facil.data[[i]]), geography == 'US'),
-                                crs = 4326)) %>% 
+        leaflet::leaflet(data = map.data) %>% 
         leaflet::addTiles() %>%
-        leaflet::fitBounds(lng1 = min(sf::st_coordinates(sf::st_as_sf(input_data$EJ.facil.data[[i]]))[,1]),
-                  lat1 = min(sf::st_coordinates(sf::st_as_sf(input_data$EJ.facil.data[[i]]))[,2]),
-                  lng2 = max(sf::st_coordinates(sf::st_as_sf(input_data$EJ.facil.data[[i]]))[,1]),
-                  lat2 = max(sf::st_coordinates(sf::st_as_sf(input_data$EJ.facil.data[[i]]))[,2])) %>%
+        leaflet::fitBounds(lng1 = min(sf::st_coordinates(map.data)[,1]),
+                           lat1 = min(sf::st_coordinates(map.data)[,2]),
+                           lng2 = max(sf::st_coordinates(map.data)[,1]),
+                           lat2 = max(sf::st_coordinates(map.data)[,2])) %>%
         leaflet::addCircleMarkers(radius = 5,
                          opacity = 0.75,
                          color = ~ pal(`Demo. indicators above 80th %ile`),
                          #clusterOptions = markerClusterOptions(),
-                         popup = leafpop::popupTable(dplyr::filter(na.omit(input_data$EJ.facil.data[[i]]), geography == geog.ind),
+                         popup = leafpop::popupTable(map.data,
                                             feature.id = F, row.numbers = F,
-                                            zcol = names(na.omit(input_data$EJ.facil.data[[i]]))[1:20])) %>%
+                                            zcol = names(map.data)[1:20])) %>%
         leaflet::addLegend(pal = pal, values = ~`Demo. indicators above 80th %ile`, position = "bottomright")
     }
 
