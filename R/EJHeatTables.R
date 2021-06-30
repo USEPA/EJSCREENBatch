8dc52731956c5041a52aecd7a6c7f3481abe1d0e
Adam Theising <52790883+adamtheising@users.noreply.github.com>
2021-06-30 14:10:34 -0400

    Update EJHeatTables.R

--- R/EJHeatTables.R
+++ R/EJHeatTables.R
@@ -159,7 +159,7 @@ EJHeatTables <- function(input_data, type, geog_lvl= NULL, keepid = NULL, topN =
         )[geography == geog & shape_ID == shape.keep,
           dplyr::select(.SD, `Low Income`:`Resp. Hazard`)
           ][, lapply(.SD, round)]
-      dt[[i]] <- dplyr::melt(dt[[i]])[,2]
+      dt[[i]] <- data.table::melt(dt[[i]])[,2]
       names(dt[[i]]) <- paste0(str_sub(labels(input_data$EJ.facil.data)[[i]],-3,-3),
                                ' mile radius')
     }
