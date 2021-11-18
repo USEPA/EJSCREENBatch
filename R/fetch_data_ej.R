dd356259144efa2a5529cf8d88c858980ed10340
arelkhattabi <arelkhattabi@gmail.com>
2021-11-18 15:18:49 -0500

    test

--- R/fetch_data_ej.R
+++ R/fetch_data_ej.R
@@ -15,14 +15,14 @@
 fetch_data_ej <- function(working_dir, state_filter){
   #first check if data folder exists
   ifelse(!dir.exists(paste0(working_dir, "/EJSCREEN data")), dir.create(paste0(working_dir, "/EJSCREEN data")), FALSE)
-  
+
   #edited function to download gdb
   options(download.file.method="libcurl")
-  
+
   # NOTE: REMOVE HI, AK, and islands for projection purposes
   #if files do not exist, go get most recent. If files do exist, open most recent.
   calendaryear <- as.numeric(format(Sys.time(), "%Y"))
-  
+
   # Option to filter states--only used if user specifies this
   filter_state <- function(data, state_filter){
     if(!is.null(state_filter)){
@@ -36,7 +36,7 @@ fetch_data_ej <- function(working_dir, state_filter){
         dplyr::filter(!(ST_ABBREV %in% c("AK","HI","GU","MP","VI","AS")))
     }
   }
-  
+
   #Block group level data and state percentiles
   if(identical(list.files(path=paste0(working_dir, "/EJSCREEN data"), pattern="StatePctile.gdb"), character(0)) ){
     #If data not downloaded, download most recent data
@@ -50,11 +50,11 @@ fetch_data_ej <- function(working_dir, state_filter){
       st_transform("ESRI:102005") %>%
       mutate(area_bg = st_area(Shape)) %>%
       rename_at(vars(starts_with("P_")), ~ paste0(., '_state'))
-    
+
   }
   st_crs(gdb_stpctile)$units
-  
-  
+
+
   #national percentiles
   if(identical(list.files(path=paste0(working_dir,"/EJSCREEN data"), pattern="USPR.csv"), character(0)) ){
     #If data not downloaded, download most recent data
@@ -66,17 +66,17 @@ fetch_data_ej <- function(working_dir, state_filter){
     calendar_year <- max(as.numeric(gsub("[^0-9]", "", list.files(path=paste0(working_dir,"/EJSCREEN data/"), pattern="USPR.csv"))))
     csv_uspr <- read_csv(paste0(working_dir,"/EJSCREEN data/EJSCREEN_",calendar_year,"_USPR.csv"), col_types=cols(.default = "c")) %>%
       dplyr::select(ID, starts_with("P_")) %>%
-      rename_at(vars(-ID), ~ paste0(., '_US')) %>%
+      rename_at(vars(-ID), ~ paste0(., '_US'))  %>%
       na_if("None") %>%
       mutate_at(vars(-ID), as.numeric)
   }
-  
-  
+
+
   data.state.uspr <- gdb_stpctile %>%
     left_join(csv_uspr, by=c("ID"="ID")) %>%
     filter_state(state_filter=state_filter) %>%
     dplyr::filter(!(ACSTOTPOP==0))
-  
+
   return(data.state.uspr)
-  
+
 }
